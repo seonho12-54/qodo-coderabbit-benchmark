e79d1ed0e3f3273fe2e98e9ea2a39fda96b43461
seonho12-54 <seonho12-54@users.noreply.github.com>
2026-09-03 10:40:04 +0900

    Normalize template names with casefold for autoescape

--- src/flask/sansio/app.py
+++ src/flask/sansio/app.py
@@ -544,7 +544,7 @@ class App(Scaffold):
         """
         if filename is None:
             return True
-        return filename.lower().endswith((".html", ".htm", ".xml", ".xhtml", ".svg"))
+        return filename.casefold().endswith((".html", ".htm", ".xml", ".xhtml", ".svg"))
 
     @property
     def debug(self) -> bool:
